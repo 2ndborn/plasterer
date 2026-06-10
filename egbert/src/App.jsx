9fcc3e608508b4ff59688337f39d7187dc9cb5e4
Micah Francis <micah.francis69@googlemail.com>
2026-06-10 08:28:34 +0100

    positioned the icons on the services page

--- egbert/src/App.jsx
+++ egbert/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import NavBar from './components/NavBar'
 import About from './pages/About'
 import Services from './pages/Services'
+import Next from './pages/Next'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Home />
       <About />
       <Services />
+      <Next />
     </>
   )
 }
---
+++ egbert/src/pages/Next.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import {motion} from 'framer-motion';
+import styles from '../styles/Next.module.css';
+import Reveal from '../utils/Reveal';
+
+import { FiPhone } from "react-icons/fi";
+import { GrSchedule } from "react-icons/gr";
+import { LuClipboardPenLine } from "react-icons/lu";
+
+const Next = () => {
+    const heading = {
+        hidden: {opacity: 0, y: 20},
+        show: {opacity: 1, y: 0}
+    }
+    const steps = [
+        {id: 1, icon: <FiPhone />, title: "Call me"},
+        {id: 2, icon: <GrSchedule />, title: "Schedule a visit"},
+        {id: 3, icon: <LuClipboardPenLine />, title: "Generate a quote"},
+    ]
+    return (
+        <div style={{
+            position: 'relative',
+            minHeight: '75vh',
+            padding: '2rem',
+            textAlign: 'center',
+            fontSize: '1.5rem'
+        }}
+        >
+            <Reveal origin='left' overlayColor='#fff'>
+            <div
+                style={{
+                    padding: '1.5rem',
+                    borderRadius: '20px',
+                    boxShadow: '3px 3px 6px rgba(0, 0, 0, 0.6), -2px 1px 8px rgba(0, 0, 0, 0.8)',
+                    background: '#464D5D',
+                    color: '#302f2f',
+                }}
+            >
+                <div>
+                    <h2 style={{color: '#fff', margin: '0 0 1.5rem 0'}}>Next Steps</h2>
+                </div>
+                <div className={styles.NextGrid}>
+                    {steps.map((step, i) => (
+                        <motion.div
+                            key={i}
+                            style={{
+                                position: 'relative',
+                                borderRadius: '15px',
+                                background: '#fff',
+                                boxShadow: '2px 2px 4px rgba(255, 255, 255, 0.6), -2px 1px 6px rgba(255, 255, 255, 0.8)',
+                                padding: '20px',
+                                overflow: 'hidden',
+                            }}
+                        >
+                            <div data-name={step.id} className={styles.InnerNext}>
+                                <div style={{fontSize: '3rem', margin: '1.5rem 0'}}>{step.icon}</div>
+                                {step.id === 1 ? (
+                                    <button 
+                                        style={{
+                                            minWidth: '150px',
+                                            padding: '0.75rem',
+                                            borderRadius: '10px',
+                                            background: '#d7fc03',
+                                            color: '#3B403E',
+                                            fontSize: '1.2rem',
+                                            fontWeight: 525,
+                                            border: 'none',
+                                            boxShadow: '0px 3px 6px rgba(255, 255, 255, 0.6)',
+                                            cursor: 'pointer',
+                                            margin: '2rem 0'
+                                        }}
+                                    >
+                                        Contact me
+                                    </button>
+                                ) : (
+                                    <h4 style={{margin: '2rem 0', lineHeight: 1.4}}>{step.title}</h4>
+                                )}
+                            </div>
+                        </motion.div>
+                    ))}
+                </div>
+            </div>
+            </Reveal>
+        </div>
+    )
+}
+
+export default Next
